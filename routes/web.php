c4c9732340fc76f48e4c6b25d3df2bb6d475ba05
Semet <hamdanilombok@gmail.com>
2023-01-29 20:27:03 +0700

    add booking routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.page');
+Route::get('/booking', [BookingController::class, 'index'])->name('booking');
